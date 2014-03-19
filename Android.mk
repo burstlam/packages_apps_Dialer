d1c86d27ea2e9588aee745064f2bf6a6e9bf8f03
xilence2210 <xilence2210@gmail.com>
2014-03-19 17:34:58 +0800

    Chinese T9 support
    Add Chinese T9 support by SmartDialMap interface.
     * Support both Chinese and Latin Contacts Name.
     * Convert Chinese by lib pinyin4android.jar from
       http://code.google.com/p/pinyin4android/
    
    Change-Id: I27aa472587ff3361a24361a3045fe76fe7d5b1ca
    
    Conflicts:
    	Android.mk

--- Android.mk
+++ Android.mk
@@ -28,6 +28,7 @@ LOCAL_STATIC_JAVA_LIBRARIES := \
     android-support-v13 \
     android-support-v4 \
     android-ex-variablespeed \
+    pinyin \
 
 LOCAL_REQUIRED_MODULES := libvariablespeed
 
@@ -38,6 +39,13 @@ LOCAL_PRIVILEGED_MODULE := true
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags $(incallui_dir)/proguard.flags
 
 include $(BUILD_PACKAGE)
+
+# include pinyin lib
+include $(CLEAR_VARS)
+
+LOCAL_PREBUILT_STATIC_JAVA_LIBRARIES := pinyin:libs/pinyin4android.jar
+
+include $(BUILD_MULTI_PREBUILT)
 
 # Use the following include to make our test apk.
 include $(call all-makefiles-under,$(LOCAL_PATH))
--- src/com/android/dialer/dialpad/SmartDialPrefix.java
+++ src/com/android/dialer/dialpad/SmartDialPrefix.java
@@ -17,7 +17,6 @@
 package com.android.dialer.dialpad;
 
 import android.content.Context;
-
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 import android.telephony.TelephonyManager;
@@ -92,7 +91,9 @@ public class SmartDialPrefix {
 
         /** Sets a layout for SmartDial depending on current UI language.*/
         String locale = context.getResources().getConfiguration().locale.getCountry();
-        if (locale.equals("RU")) {
+        if (locale.equals("CN")||locale.equals("TW")) {
+            mMap = new ChineseSmartDialMap(context);
+        } else if (locale.equals("RU")) {
             mMap = new RussianSmartDialMap();
         } else if (locale.equals("GR")) {
             mMap = new GreekSmartDialMap();
@@ -609,6 +610,7 @@ public class SmartDialPrefix {
         result.add("TC"); // Turks and Caicos Islands
         result.add("VI"); // U.S. Virgin Islands
         result.add("RU"); // Russia
+        result.add("ZH"); // China
         return result;
     }
 
